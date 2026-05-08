c1e4abbbe19908c6981656b3b0fdeff14dcdd21b
TheAlexSandro <jevringarmindo@gmail.com>
2026-05-08 15:09:41 +0700

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['carol-poet-arc-karl.trycloudflare.com']
+		allowedHosts: ['usc-nokia-relatives-packing.trycloudflare.com']
 	},
 	assetsInclude: ['**/*.lottie']
 });
